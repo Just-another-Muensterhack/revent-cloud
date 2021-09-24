f7a9f699937e29bc846f6bb4619da2e7d5629d9a
Max Schäfer <max-schaefer-lg@gmx.de>
2021-09-24 21:20:39 +0200

    add search function and fix date on index

--- functions/src/search.ts
+++ functions/src/search.ts
@@ -13,8 +13,24 @@ export const index = functions
       const authHeader = Buffer.from(`${solr.user}:${solr.pass}`)
           .toString("base64");
 
-      const data = snapshot.after.data();
-      if (data?.date) delete data.date; // TOOD: to iso
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line camelcase,max-len
+      const {organizer, title, description, date, genre, price_class} = snapshot.after.data();
+
+      const data = {
+        organizer,
+        title,
+        description,
+        date,
+        genre,
+        price_class,
+      };
+
+      if (data?.date) {
+        const date = new Date(data.date._seconds * 1000);
+        data.date = date.toISOString();
+      }
 
       const body = {
         add: {
@@ -47,5 +63,40 @@ export const search = functions
     .region("europe-west3")
     .https
     .onRequest(async (req, res) => {
-      res.sendStatus(418);
+      const {solr} = functions.config();
+      const {term, startDate, endDate, genre, priceClass} = req.body;
+
+      const authHeader = Buffer.from(`${solr.user}:${solr.pass}`)
+          .toString("base64");
+
+      const body: { query: string, filter: string[] } = {
+        query: `search_text: ${term}`,
+        filter: [],
+      };
+
+      if (startDate && endDate) {
+        body.filter.push(`date: [${startDate} TO ${endDate}]`);
+      }
+
+      if (genre) {
+        body.filter.push(`genre: (${genre.join(" OR ")})`);
+      }
+
+      if (priceClass) {
+        body.filter.push(`price_class: ${priceClass}`);
+      }
+
+      const response = await fetch(`${solr.base_url}/solr/revent/query`, {
+        method: "POST",
+        headers: {
+          "Authorization": `Basic ${authHeader}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const text = await response.text();
+      console.log(res.status, response.statusText, JSON.stringify(text));
+
+      res.json(text.response);
     });
